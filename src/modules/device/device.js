6071a6d4438e5012cb3435317815184713c1ab66
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2022-09-23 16:49:12 +0300

    feat: implement device info

--- src/modules/device/device.js
+++ src/modules/device/device.js
@@ -1,11 +1,43 @@
 import Control from "control";
 import Card from "card";
+import DeviceDetector from "device-detector-js";
 
 export default class Device extends Card {
   constructor(container) {
     super(container, "Device");
 
-    const chargePercent = new Control(this.card.node, "h3", "card__text");
+    const content = new Control(this.card.node, 'div', 'card__content');
+
+    const deviceDetector = new DeviceDetector();
+    const userAgent = navigator.userAgent;
+    const device = deviceDetector.parse(userAgent);
+
+    console.log(device);
+
+    const deviceType = new Control(
+      content.node,
+      "h3",
+      "card__text",
+      `Type: ${device.device.type}`
+    );
+    const os = new Control(
+      content.node,
+      "h3",
+      "card__text",
+      `OS: ${device.os.name} ${device.os.version}`
+    );
+        const platform = new Control(
+          content.node,
+          "h3",
+          "card__text",
+          `Platform: ${device.os.platform}`
+        );
+    const os2 = new Control(
+      content.node,
+      "h3",
+      "card__text",
+      `Browser: ${device.client.name} ${device.client.version}`
+    );
 
   }
 }
