9987a12a11aca7cbc7a673c6649af8715d55f1c8
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2022-09-23 03:22:44 +0300

    init: start project

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const baseConfig = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      // изображения
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|wav)$/i,
+        type: "asset/resource",
+      },
+      // шрифты и SVG
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+      // CSS, PostCSS, Sass
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Your secrets",
+      template: path.resolve(__dirname, "./src/template.html"), // шаблон
+      filename: "index.html", // название выходного файла
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+  resolve: {
+    extensions: [".js"],
+    alias: {
+      control$: path.resolve(__dirname, "src/modules/control.js"),
+    },
+  },
+  output: {
+    publicPath: "auto",
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === "prod";
+  const envConfig = isProductionMode
+    ? require("./webpack.prod.config")
+    : require("./webpack.dev.config");
+
+  return merge(baseConfig, envConfig);
+};
