c223e4018b1a4e41d172039309b8bbfc601e144c
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2022-09-23 15:50:54 +0300

    refactor: delete unecessary code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import "./styles/index.scss";
 import Layout from "./layout/layout";
-// import Battery from "./modules/battery/battery";
-// import Device from "./modules/device/device";
 
 const root = document.querySelector('#root');
 
 new Layout(root);
-// new Battery(root);
-// new Device(root);
