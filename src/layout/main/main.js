f62cb2901bb7cd4a2422bd7658397db371c0dd3b
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2022-09-25 22:13:20 +0300

    feat: implement full info from user agent

--- src/layout/main/main.js
+++ src/layout/main/main.js
@@ -7,7 +7,7 @@ export default class Main {
   constructor(container) {
     this.main = new Control(container, 'main', 'main');
 
-    new Battery(this.main.node);
     new Device(this.main.node);
+    new Battery(this.main.node);
   }
 }
--- src/modules/device/device.js
+++ src/modules/device/device.js
@@ -4,40 +4,140 @@ import DeviceDetector from "device-detector-js";
 
 export default class Device extends Card {
   constructor(container) {
-    super(container, "Device");
+    super(container, "User Agent");
 
-    const content = new Control(this.card.node, 'div', 'card__content');
+    const content = new Control(this.card.node, "div", "card__content");
 
     const deviceDetector = new DeviceDetector();
     const userAgent = navigator.userAgent;
     const device = deviceDetector.parse(userAgent);
 
-    console.log(device);
+    // Device
+    const [deviceType, deviceBrand, deviceModel] = [
+      device.device.type,
+      device.device.brand,
+      device.device.model,
+    ];
+    if (deviceType || deviceBrand || deviceModel) {
+      new Control(content.node, "h3", "card__text", `----- Device -----`);
+    }
+    if (deviceType) {
+      new Control(content.node, "h3", "card__text", `Type: ${deviceType}`);
+    }
+    if (deviceBrand) {
+      new Control(content.node, "h3", "card__text", `Brand: ${deviceBrand}`);
+    }
+    if (deviceModel) {
+      new Control(content.node, "h3", "card__text", `Model: ${deviceModel}`);
+    }
 
-    const deviceType = new Control(
-      content.node,
-      "h3",
-      "card__text",
-      `Type: ${device.device.type}`
-    );
-    const os = new Control(
-      content.node,
-      "h3",
-      "card__text",
-      `OS: ${device.os.name} ${device.os.version}`
-    );
-        const platform = new Control(
+    // OS
+    const [osName, osVersion, osPlatform] = [
+      device.os.name,
+      device.os.version,
+      device.os.platform,
+    ];
+    if (osName || osVersion || osPlatform) {
+      new Control(content.node, "h3", "card__text", `----- OS -----`);
+    }
+    if (osName) {
+      new Control(content.node, "h3", "card__text", `Name: ${osName}`);
+    }
+    if (osVersion) {
+      new Control(content.node, "h3", "card__text", `Version: ${osVersion}`);
+    }
+    if (osPlatform) {
+      new Control(content.node, "h3", "card__text", `Platform: ${osPlatform}`);
+    }
+
+    // Client
+    const [
+      clientType,
+      clientName,
+      clientVersion,
+      clientEngine,
+      clientEngineVersion,
+      clientURL,
+    ] = [
+      device.client.type,
+      device.client.name,
+      device.client.version,
+      device.client.engine,
+      device.client.engineVersion,
+      device.client.url,
+    ];
+    if (
+      clientType ||
+      clientName ||
+      clientVersion ||
+      clientEngine ||
+      clientEngineVersion ||
+      clientURL
+    ) {
+      new Control(content.node, "h3", "card__text", `----- Client -----`);
+    }
+    if (clientType) {
+      new Control(content.node, "h3", "card__text", `Type: ${clientType}`);
+    }
+    if (clientName) {
+      new Control(content.node, "h3", "card__text", `Name: ${clientName}`);
+    }
+    if (clientVersion) {
+      new Control(
+        content.node,
+        "h3",
+        "card__text",
+        `Version: ${clientVersion}`
+      );
+    }
+    if (clientEngine) {
+      new Control(content.node, "h3", "card__text", `Engine: ${clientEngine}`);
+    }
+    if (clientEngineVersion) {
+      new Control(
+        content.node,
+        "h3",
+        "card__text",
+        `Engine Version: ${clientEngineVersion}`
+      );
+    }
+    if (clientURL) {
+      new Control(content.node, "h3", "card__text", `URL: ${clientURL}`);
+    }
+
+    // Bot
+    if (device.bot) {
+      const [botCategory, botName, botProducer, botUrl] = [
+        device.bot.category,
+        device.bot.name,
+        device.bot.producer,
+        device.bot.url,
+      ];
+      if (botCategory || botName || botProducer || botUrl) {
+        new Control(content.node, "h3", "card__text", `----- Bot -----`);
+      }
+      if (botCategory) {
+        new Control(
           content.node,
           "h3",
           "card__text",
-          `Platform: ${device.os.platform}`
+          `Category: ${botCategory}`
         );
-    const os2 = new Control(
-      content.node,
-      "h3",
-      "card__text",
-      `Browser: ${device.client.name} ${device.client.version}`
-    );
-
+      }
+      if (botName) {
+        new Control(content.node, "h3", "card__text", `Name: ${botName}`);
+      }
+      if (botProducer) {
+        new Control(
+          content.node,
+          "h3",
+          "card__text",
+          `Producer: ${botProducer}`
+        );
+      }
+      if (botUrl) {
+        new Control(content.node, "h3", "card__text", `URL: ${botUrl}`);
+      }
+    }
   }
 }
