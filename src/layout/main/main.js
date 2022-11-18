a9b5ffa88b883a8995cd38612ba40c1d20ff33fe
Aleksey Zvagolsky <alekseyzvagolsky@gmail.com>
2022-11-18 12:01:06 +0200

    feat: implement specification card

--- src/layout/main/main.js
+++ src/layout/main/main.js
@@ -1,13 +1,15 @@
 import Control from "../../modules/tools/control";
 import Battery from "../../modules/battery/battery";
-import Device from "../../modules/device/device";
+import Agent from "../../modules/agent/agent";
+import Specification from "../../modules/specification/specification";
 
 export default class Main {
   main;
   constructor(container) {
     this.main = new Control(container, 'main', 'main');
 
-    new Device(this.main.node);
+    new Agent(this.main.node);
     new Battery(this.main.node);
+    new Specification(this.main.node);
   }
 }
---
+++ src/modules/specification/specification.js
@@ -0,0 +1,41 @@
+import Control from "control";
+import Card from "card";
+
+export default class Specification extends Card {
+  constructor(container) {
+    super(container, "Specification");
+
+    const content = new Control(this.card.node, "div", "card__content");
+    const storageManager = navigator.storage;
+    storageManager.estimate().then((data) => {
+      const quotaInBytes = data.quota;
+      const quotaInMegaBytes = (quotaInBytes / 1000000).toFixed(2);
+      const quotaInGigabytes = (quotaInMegaBytes / 1000).toFixed(2);
+      const usageInBytes = data.usage;
+      const usageInMegaBytes = (usageInBytes / 1000000).toFixed(2);
+      const usageInGigabytes = (usageInMegaBytes / 1000).toFixed(2);
+
+      const quota = new Control(
+        content.node,
+        "h3",
+        "card__text",
+        `Quota: ${quotaInGigabytes}GB`
+      );
+      quota.node.setAttribute("data-tooltip", "Nigger");
+      // <ion-icon name="information-circle-outline"></ion-icon>
+      
+      new Control(
+        content.node,
+        "h3",
+        "card__text",
+        `Usage: ${usageInGigabytes}GB`
+      );
+      new Control(
+        content.node,
+        "h3",
+        "card__text",
+        `Using percentage: ${(usageInGigabytes / quotaInGigabytes).toFixed(2)}%`
+      );
+    });
+  }
+}
